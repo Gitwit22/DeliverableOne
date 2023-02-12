15089e341403bc800c4a62fc600d86a01e29ad2d
John <itstheplugllc@gmail.com>
2023-02-12 17:15:03 -0600

    initial commit

--- DeliverableOne.java
+++ DeliverableOne.java
@@ -5,60 +5,83 @@ public class DeliverableOne {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int sodasInStock = 100;
-		int sodasSold = 0;
-		
-		int sodasInStock - sodasSold = sodasRemainig;
-		
+		int sodasSold = 0; 
+			
 		
 		System.out.println("How many sodas have been sold today? " +  sodasInStock + " in stock.");
 		sodasSold = input.nextInt();
 		
+		int sodasRemaining = sodasInStock - sodasSold;
 
 		
 		if (sodasSold > 100) {
 			System.out.println("Too High");
-			System.out.println("There are " + sodasInStock + " sodas remaining");
+			System.out.println("You have " + sodasInStock + " sodas remaining.");
+		
 		} else if (sodasSold < 100) {
-			System.out.println("You have " + (sodasRemaining) + " sodas remaining");
-					}
+			System.out.println("You have " + sodasRemaining + " sodas remaining");
+					} 
 	
 		int chipsInStock = 40;
 		int chipsSold = 0;
-		int chipsInStock - chipsSold = chipsRemainig;
+		
 		
 		System.out.println("How many chips have been sold today? " +  chipsInStock + " in stock.");
 		chipsSold = input.nextInt();
 		
-		if (chipsSold > 100) {
+		int chipsRemaining = chipsInStock - chipsSold;
+		
+		if (chipsSold > 40) {
 			System.out.println("Too High");
+			System.out.println("How many chips have been sold today? " +  chipsInStock + " in stock.");
+			chipsSold = input.nextInt();
+			
 			System.out.println("There are " + chipsInStock + " chips remaining");
-		} else if (chipsSold < 100) {
-			System.out.println("You have " + (chipsRemaining) + " chips remaining");
+		} 
+		if (chipsSold < 40) {
+			System.out.println("You have " + chipsRemaining + " chips remaining");
 					}
 		
 
 		int candyInStock = 60;
 		int candySold = 0;
-		int candyRemaining = candyInStock - candySold;
+	
 		
 		System.out.println("How many candies have been sold today? " +  candyInStock + " in stock.");
 		candySold = input.nextInt();
 		
-		if (candySold > 100) {
+		int candyRemaining = candyInStock - candySold;
+		
+		if (candySold > 60) {
 			System.out.println("Too High");
 			System.out.println("There are " + candyInStock + " candies remaining");
-		} else if (candySold < 100) {
-			System.out.println("You have " + (candyRemaining) + " candies remaining");
-			}
-
+		} else if (candySold < 60) {
+			System.out.println("You have " + candyRemaining + " candies remaining");
+		}
+		
 		int sodaRestockLimit = 40;
 		int chipRestockLimit = 20;
 		int candyRestockLimit = 40;
 		
+		System.out.println("Thanks you for entering the values!");
+		
+		System.out.println("These items need to be restocked");
+		
 		if (sodasRemaining <= sodaRestockLimit) {
-			System.out.println("You need to restock the sodas");
+			
+			System.out.println("Sodas");
+		
 		}
+		if (chipsRemaining <= chipRestockLimit) {
+			
+			System.out.println("Chips");
+		
+		if (candyRemaining <= candyRestockLimit) {
+				
+			System.out.println("Candy");
+			}
+		System.out.println("Thanks you for using the restocking calculator! Until next time!!");
 		}
-	}
+}
 }
 
